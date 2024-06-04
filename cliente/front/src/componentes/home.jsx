d8e89296a345e56a0a71d29ebca2f5280b1a2488
Dieg0MV <diegoma.v2004@gmail.com>
2024-06-04 17:21:12 -0600

    update 04/06/2024

--- cliente/front/src/componentes/home.jsx
+++ cliente/front/src/componentes/home.jsx
@@ -1,25 +1,51 @@
+import { useState } from "react";
 import "./style/home.css";
 import { Sidebar, Menu, SubMenu, MenuItem } from "react-pro-sidebar";
-import { Link } from "react-router-dom";
+
 export default function Home() {
+  const [rtl, setRtl] = useState(false);
+  const [fileName, setFilename]= useState('')
+  const[data, setData]= useState('')
 
- return(
-  <div className="content">
-    <nav>
-      <h1>Home</h1>
-    </nav>
+  const make = async (e)=>{
+    e.preventDefault();
+      try {
+        await fetch('http://localhost:4000/api/createlans',{
+          method:'POST',
+          headers:{
+            'Contet-Type':'application/json'
+          },
+          body:JSON.stringify({fileName, data})
+        }.then((res)=>{ 
+          return(res)
+        }) 
+      )
+      } catch (error) {
+        console.log("error --> ",error)
+      }
+  }
 
-    <div className="body">
-      <Sidebar className="bar">
-        <Menu>
-          <SubMenu label="Chats">
-            <MenuItem> MS </MenuItem>
-            <MenuItem> WSp </MenuItem>
-          </SubMenu>
-          <MenuItem> Dashboard </MenuItem>
-          <MenuItem component={<Link to="/maker"/>}> LandingMaker </MenuItem>
-        </Menu>
-      </Sidebar>
+
+ 
+  return(
+    <div className="content">
+      <nav className="top">
+        <h1>Home</h1>
+      </nav>
+
+      <div style={{ display: 'flex', height: '100%', direction: rtl ? 'rtl' : 'ltr' }}>
+        <Sidebar className="bar">
+          <Menu>
+            <SubMenu label="Chats">
+              <MenuItem> MS </MenuItem>
+              <MenuItem> WSp </MenuItem>
+            </SubMenu>
+            <MenuItem> Dashboard </MenuItem>
+            <MenuItem 
+            
+            > LandingMaker </MenuItem>
+          </Menu>
+        </Sidebar>
+      </div>
     </div>
-  </div>
 )}
